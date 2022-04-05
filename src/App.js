998b5b5d90486dbd451c3527996c1e72684e85b3
pyrhaa <sboubtane@protonmail.com>
2022-04-05 15:11:07 +0200

    create func fetchGreeting

--- src/App.js
+++ src/App.js
@@ -6,6 +6,29 @@ import './App.css';
 const greeterAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
 
 const App = () => {
+  const [greeting, setGreetingValue] = useState();
+
+  const reqAccount = async () => {};
+
+  const fetchGreeting = async () => {
+    if (typeof window.ethereum !== 'undefined') {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(
+        greeterAddress,
+        Greeter.abi,
+        provider
+      );
+      try {
+        const data = await contract.greet();
+        console.log('data: ', data);
+      } catch (e) {
+        console.log('Error: ', e);
+      }
+    }
+  };
+
+  const setGreeting = async () => {};
+
   return (
     <div className="App">
       <header className="App-header">
