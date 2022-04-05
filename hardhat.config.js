f85b7a11d93fc18678d10fec87b3dba23e102a4c
pyrhaa <sboubtane@protonmail.com>
2022-04-05 14:50:36 +0200

    import what we need front side

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,9 +2,8 @@ require('@nomiclabs/hardhat-waffle');
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
+task('accounts', 'Prints the list of accounts', async () => {
+  const accounts = await ethers.getSigners();
   for (const account of accounts) {
     console.log(account.address);
   }
@@ -17,13 +16,14 @@ task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: '0.8.4',
-  path: {
+  defaultNetwork: 'hardhat',
+  paths: {
     artifacts: './src/artifacts'
   },
   networks: {
     hardhat: {
       chainId: 1337
     }
-  }
+  },
+  solidity: '0.8.4'
 };
--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { ethers } from 'ethers';
+import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
 import './App.css';
 
-function App() {
+const greeterAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -13,13 +16,12 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >
+          rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
   );
-}
+};
 
 export default App;
