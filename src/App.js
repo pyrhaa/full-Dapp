324eb4422455b0c23adb2d719ffc50e5d8b253fd
pyrhaa <sboubtane@protonmail.com>
2022-04-09 19:36:07 +0200

    set func setGreeting

--- src/App.js
+++ src/App.js
@@ -8,7 +8,9 @@ const greeterAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
 const App = () => {
   const [greeting, setGreetingValue] = useState();
 
-  const reqAccount = async () => {};
+  const reqAccount = async () => {
+    await window.ethereum.request({ method: 'eth_reqestAccounts' });
+  };
 
   const fetchGreeting = async () => {
     if (typeof window.ethereum !== 'undefined') {
@@ -27,7 +29,23 @@ const App = () => {
     }
   };
 
-  const setGreeting = async () => {};
+  const setGreeting = async () => {
+    if (!greeting) {
+      if (typeof window.ethereum !== 'undefined') {
+        await reqAccount();
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const contract = new ethers.Contract(
+          greeterAddress,
+          Greeter.abi,
+          signer
+        );
+        const transaction = await contract.setGreeting(greeting);
+        await transaction.wait();
+        fetchGreeting();
+      }
+    }
+  };
 
   return (
     <div className="App">
