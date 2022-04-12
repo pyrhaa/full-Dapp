b23ea8f711ed1edd5ab557a5ff92ebdda63da53f
pyrhaa <sboubtane@protonmail.com>
2022-04-12 14:24:29 +0200

    import token and his address in front part

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import { ethers } from 'ethers';
 import Greeter from './artifacts/contracts/Greeter.sol/Greeter.json';
+import Token from './artifacts/contracts/Token.sol/Token.json';
 import './App.css';
 
 const greeterAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+const tokenAddress = '0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512';
 
 const App = () => {
   const [greeting, setGreetingValue] = useState('');
